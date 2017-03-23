db737e5e20a1558c020795578891dbd8b7744ede
Paul Laliberte <pala5552@colorado.edu>
2017-03-22 23:47:13 -0600

    o

--- sa_algo.py
+++ sa_algo.py
@@ -3,8 +3,6 @@
 import argparse
 from sympy import isprime
 
-#TODO: finish functions that had done before, complete algo by next friday/convert to c++
-
 class sa_algo:
 
     def __init__(self, radius, x_coor, y_coor, prime):
@@ -18,6 +16,11 @@ class sa_algo:
         self.e_d = None
         self.e_bp = None
         self.e_dp = None
+
+        #d_p
+        self.d_p = None
+
+        #d, b_p
 
 
     def check_conditions(self):
@@ -38,20 +41,39 @@ class sa_algo:
         else:
             e_r = self.r / self.p
             e_x = self.x / self.p
-            self.e_bp, self.e_dp = min([e_r, e_x])
+            self.e_d = min([e_r, e_x])
 
     def find_ebp_edp(self):
-        #already defined - update later
-        pass
-
+        if self.y % self.p != 0:
+            self.e_bp = 0
+            self.e_dp = 0
+        else:
+            e_r = self.r / self.p
+            e_x = self.x / self.p
+            self.e_bp = min([e_r, e_x])
+            self.e_dp = self.e_bp
+            
     def find_dp(self):
-        #already defined - update later 
-        pass
+        iterations = self.r /self.p
+        p_to_edp = self.p ** self.e_dp
+        while iterations > 1:
+            p_to_edp *= p_to_edp
+            iterations -= 1
+
+        self.d_p = p_to_edp
 
     def solve_congruence_1(self):
-        #define
+        #-a mod b = b - (a mod b) ....
+        pass
+
+    def solve_congruence_2(self):
         pass
 
 
 if __name__ == '__main__':
-    pass
+    n = sa_algo(3,2,1,3)
+    n.check_conditions()
+    n.find_ed()
+    n.find_ebp_edp()
+    n.find_dp()
+    print n.d
